b2ce3f8eacaa7e47889a8d49e22c0edb6b2d3cea
Anish.M.Prasad <anish.m.prasad@gmail.com>
2020-04-08 00:04:31 +0530

    BIM-1914

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 const jsondata = require("./aakash.json");
-const aimt = require("./aimt.json");
+const ntadata = require("./nta.json");
 // const omr = require("./ckeditor");
 
 // console.log(data);
@@ -17,13 +17,13 @@ const aimt = require("./aimt.json");
 app.use(
   cors({
     credentials: true,
-    origin: "http://development.embibe.com"
+    origin: "http://development.embibe.com",
   })
 );
 app.use("/data", data);
-app.use("/aimt", aimtx);
+app.use("/nta", nta);
 
-app.use(function(req, res) {
+app.use(function (req, res) {
   res.status(404).send({ url: req.originalUrl + " not found" });
 });
 
@@ -38,18 +38,18 @@ function data(req, res) {
     .cookie("cookiename", "cookievalue", {
       maxAge: 900000,
       domain: "development.embibe.com",
-      path: "/"
+      path: "/",
     })
     .status(200)
     .send(jsondata);
 }
-function aimtx(req, res) {
+function nta(req, res) {
   return res
     .cookie("cookiename", "cookievalue", {
       maxAge: 900000,
       domain: "development.embibe.com",
-      path: "/"
+      path: "/",
     })
     .status(200)
-    .send(aimt);
+    .send(ntadata);
 }
